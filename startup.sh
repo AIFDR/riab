31b50f888e0ca6e7fb6570f02350cdf8e19514d4
Ariel Nunez <ingenieroariel@shiva.(none)>
2011-03-25 14:01:44 +0700

    Fixed startup script with the new convention

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
-apache-tomcat-6.0.32/bin/catalina.sh start
+tomcat/bin/catalina.sh start
 paster serve --reload riab_geonode/riab/deploy/project.paste.ini
-apache-tomcat-6.0.32/bin/catalina.sh stop
+tomcat/bin/catalina.sh stop
