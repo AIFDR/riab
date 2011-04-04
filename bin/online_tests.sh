0ba88db18a89993155f87504295d2eee2433a491
Ariel Nunez <ingenieroariel@shiva.(none)>
2011-04-04 09:52:56 +0700

    Kill the django server too in stop_all.py and added admin/admin user for development and demo

--- bin/online_tests.sh
+++ bin/online_tests.sh
@@ -1,6 +1,9 @@
 ./riab/bin/clean.sh
 tomcat/bin/catalina.sh start
 django-admin.py syncdb --noinput
+paster serve --reload riab_geonode/riab/deploy/project.paste.ini &
 cd riab_geonode/riab
-./manage.py test
-tomcat/bin/catalina.sh stop
+./manage.py test calculations --verbosity=2 --failfast
+cd ../../
+django-admin.py loaddata riab/bin/users.json
+#./stop_all.py
