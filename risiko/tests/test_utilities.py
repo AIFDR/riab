83f9a24f9142b10aed75f86cb4760aafbf5c8f50
Ariel Nunez <ingenieroariel@gmail.com>
2011-04-05 14:58:50 +0700

    Fixing the tests for batch upload

--- risiko/tests/test_utilities.py
+++ risiko/tests/test_utilities.py
@@ -1,28 +1,10 @@
-import unittest
-import numpy
 import os
-
-data_server_url = '203.77.224.75/riab/'
-test_data = 'RISIKO_test_data.tgz'
-
-os.environ["DJANGO_SETTINGS_MODULE"] = "risiko.settings"
-
-
-import sys, os, string, os
-from geonode.maps.models import Layer
+import unittest
 from geonode.maps.utils import batch_upload
 from impact.storage.io import metadata
 from django.conf import settings
-import urllib2
-import logging
 
-# Set up logging
-for _module in ['riab.utilities', 'geonode.maps.views', 'geonode.maps.gs_helpers']:
-    _logger = logging.getLogger(_module)
-    _logger.addHandler(logging.StreamHandler())
-    # available levels: DEBUG, INFO, WARNING, ERROR, CRITICAL.
-    # The earlier a level appears in this list, the more output it will produce in the log file.
-    _logger.setLevel(logging.INFO)
+TEST_DATA=os.path.join(os.environ['RIAB_HOME'], 'riab_data', 'risiko_demo_data')
 
 
 class Test_utilities(unittest.TestCase):
@@ -30,17 +12,12 @@ class Test_utilities(unittest.TestCase):
     """
 
     def setUp(self):
-            # Fetch example data
-        if not os.path.exists(os.path.join('/tmp', test_data)):
-            cmd = 'cd /tmp; wget -c %s' % os.path.join(data_server_url, test_data)
-            os.system(cmd)
+        self.datadir = TEST_DATA
 
-            cmd = 'cd /tmp; tar xvfz %s' % test_data
-            os.system(cmd)
-        self.datadir = os.path.join('/tmp', test_data)
 
     def tearDown(self):
         pass
+
 
     def test_layer_upload(self):
         """Test that layers can be uploaded to running GeoNode/GeoServer
@@ -53,7 +30,16 @@ class Test_utilities(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    import logging
 
+    os.environ["DJANGO_SETTINGS_MODULE"] = "risiko.settings"
+
+    # Set up logging
+    for _module in ['geonode.maps.utils']:
+        _logger = logging.getLogger(_module)
+        _logger.addHandler(logging.StreamHandler())
+        # available levels: DEBUG, INFO, WARNING, ERROR, CRITICAL.
+        _logger.setLevel(logging.DEBUG)
 
     suite = unittest.makeSuite(Test_utilities, 'test')
     runner = unittest.TextTestRunner(verbosity=2)
