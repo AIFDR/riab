aca4d28961949b6b6d0200ced56e9c3c2b23c7b7
Ariel Nunez <ingenieroariel@gmail.com>
2011-04-05 14:33:51 +0700

    Started moving GeoNode code to its own repo

--- risiko/tests/test_utilities.py
+++ risiko/tests/test_utilities.py
@@ -5,13 +5,13 @@ import os
 data_server_url = '203.77.224.75/riab/'
 test_data = 'RISIKO_test_data.tgz'
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "riab.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "risiko.settings"
 
 
 import sys, os, string, os
-from riab import utilities
 from geonode.maps.models import Layer
-from riab_server.webapi.storage import metadata, login
+from geonode.maps.utils import batch_upload
+from impact.storage.io import metadata
 from django.conf import settings
 import urllib2
 import logging
@@ -25,31 +25,6 @@ for _module in ['riab.utilities', 'geonode.maps.views', 'geonode.maps.gs_helpers
     _logger.setLevel(logging.INFO)
 
 
-def get_web_page(url, username=None, password=None):
-    """Get url page possible with username and password
-    """
-
-    if username is not None:
-
-        # Create password manager
-        passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        passman.add_password(None, url, username, password)
-
-        # create the handler
-        authhandler = urllib2.HTTPBasicAuthHandler(passman)
-        opener = urllib2.build_opener(authhandler)
-        urllib2.install_opener(opener)
-
-    try:
-        pagehandle = urllib2.urlopen(url)
-    except urllib2.URLError, e:
-        msg = 'Could not open URL "%s": %s' % (url, e)
-        raise urllib2.URLError(msg)
-    else:
-        page = pagehandle.readlines()
-
-    return page
-
 class Test_utilities(unittest.TestCase):
     """Tests riab_geonode utilities
     """
@@ -62,6 +37,7 @@ class Test_utilities(unittest.TestCase):
 
             cmd = 'cd /tmp; tar xvfz %s' % test_data
             os.system(cmd)
+        self.datadir = os.path.join('/tmp', test_data)
 
     def tearDown(self):
         pass
@@ -69,60 +45,12 @@ class Test_utilities(unittest.TestCase):
     def test_layer_upload(self):
         """Test that layers can be uploaded to running GeoNode/GeoServer
         """
+        uploaded = batch_upload(self.datadir)
+        for item in uploaded:
+            errors = 'errors' in item
+            msg = 'Could not upload %s. ' % item['file']
+            assert errors == False, msg + 'Error was: %s' % item['errors']
 
-        basename, _ = os.path.splitext(test_data)
-        datadir = os.path.join('/tmp', basename)
-        for subdir in os.listdir(datadir):
-            subdir = os.path.join(datadir, subdir)
-
-            if os.path.isdir(subdir):
-
-                for filename in os.listdir(subdir):
-
-                    basename, extension = os.path.splitext(filename)
-
-                    if extension in ['.asc', '.txt', '.tif', '.shp', '.zip']:
-                        layer = utilities.file_upload('%s/%s' % (subdir, filename),
-                                                           title=basename)
-                        msg = ('The name of the upload file is %s' % layer.name)
-
-                        # Verify the layer was saved:
-                        saved_layer = Layer.objects.get(name=layer.name)
-
-                        # Check the layer is in the geonode server by accessing it's url
-
-                        # Check the layer is in the geoserver by accessing it's url
-			found = False
-                        gs_username, gs_password = settings.GEOSERVER_CREDENTIALS
-			page = get_web_page(os.path.join(settings.GEOSERVER_BASE_URL, 'rest/layers'),
-                                                         username=gs_username,
-                                                         password=gs_password)
-                        for line in page:
-                            if line.find('rest/layers/%s.html' % layer.name) > 0:
-                                found = True
-
-                        msg = 'Did not find layer %s in geoserver %s' % (layer.name, settings.GEOSERVER_BASE_URL)
-#                        import ipdb;ipdb.set_trace()
-                        assert found, msg
-
-                        # See if the layer is added to the GeoServer catalog, either via WFS or WCS
-                        layer_metadata = metadata(settings.GEOSERVER_BASE_URL + 'ows', layer.typename)
-
-
-                        keywords_file = os.path.join(subdir, basename + '.keywords')
-                        if os.path.exists(keywords_file):
-                            f = open(keywords_file, 'r')
-                            keywords = [x.strip() for x in f.readlines()]
-                            for k in keywords:
-                                raw_key, raw_val = k.split(':')
-                                val = raw_val.strip()
-                                key = raw_key.strip()
-                                msg = ("Key '%s' is not in layer metadata dict for layer %s."
-                                       " Present keys are: (%s)" % (key, filename, layer_metadata.keys()))
-                                assert key in layer_metadata, msg
-                                actual_value = layer_metadata[key].strip()
-                                msg = "Expected %s but got %s" % (val, actual_value)
-                                assert actual_value == val, msg
 
 if __name__ == '__main__':
 
