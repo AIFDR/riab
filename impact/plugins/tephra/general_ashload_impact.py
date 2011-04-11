0071094898fc3830ffa0d2f6eb956f62026a5e0c
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2011-04-11 13:31:18 +0700

    Added beginnings of tephra impact model

---
+++ impact/plugins/tephra/general_ashload_impact.py
@@ -0,0 +1,53 @@
+
+import numpy
+import scipy
+
+from django.template.loader import render_to_string
+from impact.plugins.core import FunctionProvider
+
+from impact.engine.utilities import MAXFLOAT
+from impact.plugins.utilities import Damage_curve
+
+
+class TephraImpactFunction(FunctionProvider):
+    """Risk plugin for tephra damage (FIXME: Origin?)
+
+    :param requires category=="hazard" and subcategory.startswith("tephra") and layerType=="raster"
+    :param requires category=="exposure" and subcategory.startswith("building") and layerType=="feature"
+    """
+
+    @staticmethod
+    def run(hazard_layers, exposure_layers):
+        """Risk plugin for tephra impact
+        """
+
+        coordinates, attributes = hazard_layers
+        coordinates, _ = exposure_layers
+
+        N = len(attributes)
+
+        result = []
+        for i in range(N):
+
+            #-------------------
+            # Extract parameters
+            #-------------------
+            load = float(attributes[i].values()[0])
+
+            #------------------------
+            # Compute damage level
+            #------------------------
+            if 0.01 <= load < 90.0:
+                impact = 1
+            elif 90.0 <= load < 150.0:
+                impact = 2
+            elif 150.0 <= load < 300.0:
+                impact = 3
+            elif load >= 300.0:
+                impact = 4
+            else:
+                impact = 0
+
+            result.append({'Impact': impact})
+
+        return result
