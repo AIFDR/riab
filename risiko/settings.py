c4cd2e5d63ad7007c022f4175252f3be0b4bdf0a
Ariel Nunez <ingenieroariel@gmail.com>
2011-06-06 17:12:23 -0700

    Removed outdated template loader setting

--- risiko/settings.py
+++ risiko/settings.py
@@ -107,13 +107,6 @@ ADMIN_MEDIA_PREFIX = os.path.join(STATIC_URL, 'admin/')
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'myv-y4#7j-d*p-__@j#*3z@!y24fz8%^z2v6atuy4bo9vqr1_a'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
-)
-
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.auth',
     'django.core.context_processors.debug',
