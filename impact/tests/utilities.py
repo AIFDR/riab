37ab89926f25eefedc496a3d07fb6de0399585c6
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2011-06-21 11:20:08 +0700

    Factored test of layer bounding box into separate function

--- impact/tests/utilities.py
+++ impact/tests/utilities.py
@@ -1,8 +1,11 @@
 import os
 import types
+import numpy
 from django.conf import settings
+from impact.storage.io import get_bounding_box
 
 TESTDATA = os.path.join(os.environ['RIAB_HOME'], 'risiko_test_data')
+DEMODATA = os.path.join(os.environ['RIAB_HOME'], 'risiko_demo_data')
 
 # Use the local GeoServer url inside GeoNode
 # The ows bit at the end if VERY important because
@@ -46,3 +49,38 @@ def same_API(X, Y, exclude=None):
     _same_API(Y, X, exclude=exclude)
 
     return True
+
+def assert_bounding_box_matches(layer, filename):
+    """Verify that GeoNode layer has the same bounding box as filename
+    """
+
+    # Check integrity
+
+    assert hasattr(layer, 'geographic_bounding_box')
+    assert isinstance(layer.geographic_bounding_box, basestring)
+
+    # Exctract bounding bounding box from layer handle
+    s = 'POLYGON(('
+    i = layer.geographic_bounding_box.find(s) + len(s)
+    assert i > len(s)
+
+    j = layer.geographic_bounding_box.find('))')
+    assert j > i
+
+    bbox_string = str(layer.geographic_bounding_box[i:j])
+    A = numpy.array([[float(x[0]), float(x[1])] for x in
+                     (p.split() for p in bbox_string.split(','))])
+    south = min(A[:, 1])
+    north = max(A[:, 1])
+    west = min(A[:, 0])
+    east = max(A[:, 0])
+    bbox = [west, south, east, north]
+
+    # Check correctness of bounding box against reference
+    ref_bbox = get_bounding_box(filename)
+
+    msg = ('Bounding box from layer handle "%s" was not as expected.\n'
+           'Got %s, expected %s' % (layer.name, bbox, ref_bbox))
+    assert numpy.allclose(bbox, ref_bbox, rtol=1.0e-6, atol=1.0e-8), msg
+
+
