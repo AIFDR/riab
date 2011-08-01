17a8b90526587d6ac3bbf1ed9e8861ddc05eac93
Ted <ted@biometix.com>
2011-08-01 12:26:27 +0700

    Pandang updates to tests and Pep8 compliance

--- risiko/tests/test_padang.py
+++ risiko/tests/test_padang.py
@@ -21,16 +21,48 @@ from impact.tests.utilities import assert_bounding_box_matches, check_layer
 from impact.tests.utilities import TESTDATA, DEMODATA, INTERNAL_SERVER_URL
 
 
-def lembang_damage_function(x):
-    if x < 6.0:
-        value = 0.0
-    else:
-        value = (0.692 * (x ** 4) -
-                 15.82 * (x ** 3) +
-                 135.0 * (x ** 2) -
-                 509.0 * x +
-                 714.4)
-    return value
+def pandang_check_results(mmi, building_class):
+    """Check calculated results through a lookup table
+    returns False if the lookup fails and
+    an exception if more than one lookup returned"""
+
+    padang_verified_results = [
+          [7.400580, 8, 0.419928],
+          [7.402703, 8, 0.425026],
+          [7.397977, 8, 0.413749],
+          [7.450592, 8, 0.555220],
+          [7.455762, 8, 0.571148],
+          [7.453757, 8, 0.564922],
+          [7.445377, 8, 0.539542],
+          [7.433962, 8, 0.506559],
+          [7.424470, 8, 0.480477],
+          [7.400533, 8, 0.419816],
+          [7.439781, 8, 0.523150],
+          [7.398985, 8, 0.416132],
+          [7.427594, 9, 1.050452],
+          [7.435351, 8, 0.510478],
+          [7.446795, 8, 0.543766],
+          [7.454428, 8, 0.567000],
+          [7.470334, 6, 4.082062],
+          [7.401520, 8, 0.422178],
+          [7.397964, 8, 0.413719],
+          [7.396476, 8, 0.410224],
+          ]
+
+    impact_array = [verified_impact
+        for verified_mmi, verified_building_class, verified_impact
+               in padang_verified_results
+                    if numpy.allclose(verified_mmi, mmi, rtol=1.0e-6) and
+                    numpy.allclose(verified_building_class, building_class,
+                                   rtol=1.0e-6)]
+     #print impact_array, len(impact_array)
+    if len(impact_array) == 0:
+        return False
+    elif len(impact_array) == 1:
+        return impact_array[0]
+
+    assert len(impact_array) < 2, 'More than one lookup " \
+                    "result returned. May be precision error.'
 
 
 class Test_calculations(unittest.TestCase):
@@ -39,30 +71,28 @@ class Test_calculations(unittest.TestCase):
 
     def setUp(self):
         """Create valid superuser
-        """
+          """
         self.user = get_valid_user()
-
-
-
 
     def test_padang_building_examples(self):
         """Padang building impact calculation works through the API
         """
 
-        # Test for a range of hazard layers
+          # Test for a range of hazard layers
 
         for mmi_filename in ['Shakemap_Padang_2009.asc']:
-                             #'Lembang_Earthquake_Scenario.asc']:
+                               #'Lembang_Earthquake_Scenario.asc']:
 
             # Upload input data
             hazardfile = os.path.join(DEMODATA, 'hazard', mmi_filename)
             hazard_layer = save_to_geonode(hazardfile, user=self.user)
-            hazard_name = '%s:%s' % (hazard_layer.workspace, hazard_layer.name)
+            hazard_name = '%s:%s' % (hazard_layer.workspace,
+                                        hazard_layer.name)
 
             exposurefile = os.path.join(TESTDATA, 'Padang_WSG84.shp')
             exposure_layer = save_to_geonode(exposurefile, user=self.user)
             exposure_name = '%s:%s' % (exposure_layer.workspace,
-                                       exposure_layer.name)
+                                          exposure_layer.name)
 
             # Call calculation routine
 
@@ -71,86 +101,85 @@ class Test_calculations(unittest.TestCase):
             #              nicely with it - also do this in download()
             bbox = '96.956, -5.51, 104.63933, 2.289497'
 
-            #print
-            print get_bounding_box(hazardfile)
-            #print get_bounding_box(exposurefile)
-
-	    #bbox = get_bounding_box(hazardfile)
-
             with warnings.catch_warnings():
                 warnings.simplefilter('ignore')
 
                 c = Client()
                 rv = c.post('/api/v1/calculate/', data=dict(
-                        hazard_server=INTERNAL_SERVER_URL,
-                        hazard=hazard_name,
-                        exposure_server=INTERNAL_SERVER_URL,
-                        exposure=exposure_name,
-                        bbox=bbox,
-                        impact_function='Padang Earthquake Building Damage Function',
-                        keywords='test,buildings,padang',
-                        ))
+                            hazard_server=INTERNAL_SERVER_URL,
+                            hazard=hazard_name,
+                            exposure_server=INTERNAL_SERVER_URL,
+                            exposure=exposure_name,
+                            bbox=bbox,
+                            impact_function='Padang Earthquake ' \
+                                            'Building Damage Function',
+                            keywords='test,buildings,padang',
+                            ))
 
-            self.assertEqual(rv.status_code, 200)
-            self.assertEqual(rv['Content-Type'], 'application/json')
-            data = json.loads(rv.content)
-            assert 'hazard_layer' in data.keys()
-            assert 'exposure_layer' in data.keys()
-            assert 'run_duration' in data.keys()
-            assert 'run_date' in data.keys()
-            assert 'layer' in data.keys()
+                self.assertEqual(rv.status_code, 200)
+                self.assertEqual(rv['Content-Type'], 'application/json')
+                data = json.loads(rv.content)
+                assert 'hazard_layer' in data.keys()
+                assert 'exposure_layer' in data.keys()
+                assert 'run_duration' in data.keys()
+                assert 'run_date' in data.keys()
+                assert 'layer' in data.keys()
 
-            # Download result and check
-            layer_name = data['layer'].split('/')[-1]
+                # Download result and check
+                layer_name = data['layer'].split('/')[-1]
 
-            result_layer = download(INTERNAL_SERVER_URL,
-                                    layer_name,
-                                    bbox)
-            assert os.path.exists(result_layer.filename)
+                result_layer = download(INTERNAL_SERVER_URL,
+                                       layer_name,
+                                       bbox)
+                assert os.path.exists(result_layer.filename)
 
-            # Read hazard data for reference
-            hazard_raster = read_layer(hazardfile)
-            A = hazard_raster.get_data()
-            mmi_min, mmi_max = hazard_raster.get_extrema()
+                # Read hazard data for reference
+                hazard_raster = read_layer(hazardfile)
+                A = hazard_raster.get_data()
+                mmi_min, mmi_max = hazard_raster.get_extrema()
 
-            # Read calculated result
-            impact_vector = read_layer(result_layer.filename)
-            coordinates = impact_vector.get_geometry()
-            attributes = impact_vector.get_data()
+                # Read calculated result
+                impact_vector = read_layer(result_layer.filename)
+                coordinates = impact_vector.get_geometry()
+                attributes = impact_vector.get_data()
 
-            # Verify calculated result
-            count = 0
-            for i in range(len(attributes)):
-                lon, lat = coordinates[i][:]
-                calculated_mmi = attributes[i]['MMI']
+                # Verify calculated result
+                count = 0
+                verified_count = 0
+                for i in range(len(attributes)):
+                    lon, lat = coordinates[i][:]
+                    calculated_mmi = attributes[i]['MMI']
 
-                if calculated_mmi == 0.0:
-                    # FIXME (Ole): Some points have MMI==0 here.
-                    # Weird but not a show stopper
-                    continue
+                    if calculated_mmi == 0.0:
+                         # FIXME (Ole): Some points have MMI==0 here.
+                         # Weird but not a show stopper
+                        continue
 
-                # Check that interpolated points are within range
-                msg = ('Interpolated mmi %f was outside extrema: '
-                       '[%f, %f] at location '
-                       '[%f, %f]. ' % (calculated_mmi,
-                                       mmi_min, mmi_max,
-                                       lon, lat))
-                assert mmi_min <= calculated_mmi <= mmi_max, msg
+                    # Check that interpolated points are within range
+                    msg = ('Interpolated mmi %f was outside extrema: '
+                           '[%f, %f] at location '
+                           '[%f, %f]. ' % (calculated_mmi,
+                                           mmi_min, mmi_max,
+                                           lon, lat))
+                    assert mmi_min <= calculated_mmi <= mmi_max, msg
 
-                # Check calculated damage
-                calculated_dam = attributes[i]['Percent_da']
+                    building_class = attributes[i]['Building_C']
+                    # Check calculated damage
+                    calculated_dam = attributes[i]['Percent_da']
+                    verified_dam = pandang_check_results(calculated_mmi,
+                                                         building_class)
+                    if verified_dam != False:
 
-                ref_dam = lembang_damage_function(calculated_mmi)
-                msg = ('Calculated damage was not as expected '
-                       'for hazard layer %s' % hazardfile)
-                assert numpy.allclose(calculated_dam, ref_dam,
-                                      rtol=1.0e-12), msg
+                        msg = ('Calculated damage was not as expected '
+                                 'for hazard layer %s' % hazardfile)
+                        assert numpy.allclose(calculated_dam, verified_dam,
+                                               rtol=1.0e-4), msg
+                        verified_count += 1
+                    count += 1
 
-                count += 1
-
-            # Make only a few points were 0
-            assert count > len(attributes) - 4
-
+                assert verified_count == 21, 'Number of points verified'\
+                                             ' in output was not 21.'
+                assert count == 3802, 'Number buildings was not 3802.'
 
 
 if __name__ == '__main__':
