6f85ff2b6ffbc192abf9ef5b67b1c67df3b8ffa5
ahocevar <andreas.hocevar@gmail.com>
2011-08-10 17:26:44 +0200

    bringing in unrefactored code from original calculator

---
+++ calculator/app/static/script/app/locale/en.js
@@ -0,0 +1,14 @@
+GeoExt.lang.add("en", {
+    "Risiko.prototype": {
+        hazardComboLabelText: "Hazard",
+        exposureComboLabelText: "Exposure",
+        functionComboLabelText: "Function",
+        resetButtonText: "Reset",
+        calculateButtonText: "Calculate",
+        calculatingText: "Calculating",
+        calculatorTitleText: "Impact Calculator",
+        hazardSelectText: "Select Hazard ...",
+        exposureSelectText: "Select Exposure ...",
+        functionSelectText: "Select Function ..."
+    }
+});
