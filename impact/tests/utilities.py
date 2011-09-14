ffbb0c1933dee223fc6e517f880c2908a49517dd
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2011-09-14 15:26:40 +0700

    Tested and fixed numerical accuracy issues with centroid calculation which manifested at the building footprint level. Solved by normalisation and closing issue #140

--- impact/tests/utilities.py
+++ impact/tests/utilities.py
@@ -17,7 +17,9 @@ FEATURE_COUNTS = {'lembang_schools.shp': 144,
                   'tsunami_exposure_BB.shp': 7529,
                   'kecamatan_geo.shp': 42,
                   'Padang_WGS84.shp': 3896,
-                  'OSM_building_polygons_20110905.shp': 34960}
+                  'OSM_building_polygons_20110905.shp': 34960,
+                  'OSM_subset.shp': 79}
+
 
 def _same_API(X, Y, exclude=None):
     """Check that public methods of X also exist in Y
@@ -89,3 +91,47 @@ def get_web_page(url, username=None, password=None):
         page = pagehandle.readlines()
 
     return page
+
+
+def centroid_formula(P):
+    """Naive implementation of centroid formula
+
+    Input
+        P: Polygon
+
+
+    NOTE: This is not used for anything. Also it does not normalise
+          the input so is prone to rounding errors.
+    """
+
+    P = numpy.array(P)
+
+    msg = ('Polygon is assumed to consist of coordinate pairs. '
+           'I got second dimension %i instead of 2' % P.shape[1])
+    assert P.shape[1] == 2, msg
+    N = P.shape[0] - 1
+
+    x = P[:, 0]
+    y = P[:, 1]
+
+    # Area: 0.5 sum_{i=0}^{N-1} (x_i y_{i+1} - x_{i+1} y_i)
+    A = 0.0
+    for i in range(N):
+        A += x[i] * y[i+1] - x[i+1] * y[i]
+    A = A/2
+
+    # Centroid: sum_{i=0}^{N-1} (x_i + x_{i+1})(x_i y_{i+1} - x_{i+1} y_i)/(6A)
+    Cx = 0.0
+    for i in range(N):
+        Cx += (x[i] + x[i+1]) * (x[i] * y[i+1] - x[i+1] * y[i])
+    Cx = Cx/6/A
+
+
+    Cy = 0.0
+    for i in range(N):
+        Cy += (y[i] + y[i+1]) * (x[i] * y[i+1] - x[i+1] * y[i])
+    Cy = Cy/6/A
+
+
+    return [Cx, Cy]
+
