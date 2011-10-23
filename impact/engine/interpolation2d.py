20b8a2dbed542fa06d42f828f723a4c0e4e1a27e
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2011-10-23 19:11:08 +0700

    Documented derivation of interpolation formulas and updated notation in function

--- impact/engine/interpolation2d.py
+++ impact/engine/interpolation2d.py
@@ -8,18 +8,19 @@ This module
 * handles missing values in domain sensibly using NaN
 * is unit tested with a range of common and corner cases
 
+See end of this file for documentation of the mathematical derivation used.
 """
 
 import numpy
 
 
-def interpolate2d(x, y, A, points, mode='linear', bounds_error=False):
+def interpolate2d(x, y, Z, points, mode='linear', bounds_error=False):
     """Fundamental 2D interpolation routine
 
     Input
         x: 1D array of x-coordinates of the mesh on which to interpolate
         y: 1D array of y-coordinates of the mesh on which to interpolate
-        A: 2D array of values for each x, y pair
+        Z: 2D array of values for each x, y pair
         points: Nx2 array of coordinates where interpolated values are sought
         mode: Determines the interpolation order. Options are
               'constant' - piecewise constant nearest neighbour interpolation
@@ -36,7 +37,7 @@ def interpolate2d(x, y, A, points, mode='linear', bounds_error=False):
         Input coordinates x and y are assumed to be monotonically increasing,
         but need not be equidistantly spaced.
 
-        A is assumed to have dimension M x N, where M = len(x) and N = len(y).
+        Z is assumed to have dimension M x N, where M = len(x) and N = len(y).
         In other words it is assumed that the x values follow the first
         (vertical) axis downwards and y values the second (horizontal) axis
         from left to right.
@@ -50,7 +51,7 @@ def interpolate2d(x, y, A, points, mode='linear', bounds_error=False):
     """
 
     # Input checks
-    x, y, A, xi, eta = check_inputs(x, y, A, points, mode, bounds_error)
+    x, y, Z, xi, eta = check_inputs(x, y, Z, points, mode, bounds_error)
 
     # Identify elements that are outside interpolation domain or NaN
     outside = (xi < x[0]) + (eta < y[0]) + (xi > x[-1]) + (eta > y[-1])
@@ -78,41 +79,41 @@ def interpolate2d(x, y, A, points, mode='linear', bounds_error=False):
     y0 = y[idy - 1]
     y1 = y[idy]
 
-    A00 = A[idx - 1, idy - 1]
-    A01 = A[idx - 1, idy]
-    A10 = A[idx, idy - 1]
-    A11 = A[idx, idy]
+    z00 = Z[idx - 1, idy - 1]
+    z01 = Z[idx - 1, idy]
+    z10 = Z[idx, idy - 1]
+    z11 = Z[idx, idy]
 
     # Linear interpolation formula
     alpha = (xi - x0) / (x1 - x0)
     beta = (eta - y0) / (y1 - y0)
 
-    Dx = A10 - A00
-    Dy = A01 - A00
+    dx = z10 - z00
+    dy = z01 - z00
 
-    Z = A00 + alpha * Dx + beta * Dy + alpha * beta * (A11 - Dx - Dy - A00)
+    z = z00 + alpha * dx + beta * dy + alpha * beta * (z11 - dx - dy - z00)
 
     # Self test
-    if len(Z) > 0:
+    if len(z) > 0:
+        mz = numpy.nanmax(z)
         mZ = numpy.nanmax(Z)
-        mA = numpy.nanmax(A)
         msg = ('Internal check failed. Max interpolated value %.15f '
-               'exceeds max grid value %.15f ' % (mZ, mA))
-        assert mZ <= mA, msg
+               'exceeds max grid value %.15f ' % (mz, mZ))
+        assert mz <= mZ, msg
 
     # Populate result with interpolated values for points inside domain
     # and NaN for values outside
-    R = numpy.zeros(len(points))
-    R[inside] = Z
-    R[outside] = numpy.nan
+    r = numpy.zeros(len(points))
+    r[inside] = z
+    r[outside] = numpy.nan
 
-    return R
+    return r
 
 
-def interpolate_raster(x, y, A, points, mode='linear', bounds_error=False):
+def interpolate_raster(x, y, Z, points, mode='linear', bounds_error=False):
     """2D interpolation of raster data
 
-    It is assumed that data is organised in A as latitudes from
+    It is assumed that data is organised in matrix Z as latitudes from
     bottom up along the first dimension and longitudes from west to east
     along the second dimension.
 
@@ -122,19 +123,19 @@ def interpolate_raster(x, y, A, points, mode='linear', bounds_error=False):
     See interpolate2d for details of the interpolation routine
     """
 
-    # Flip matrix A up-down so that scipy will interpret latitudes correctly.
-    A = numpy.flipud(A)
+    # Flip matrix Z up-down so that scipy will interpret latitudes correctly.
+    Z = numpy.flipud(Z)
 
-    # Transpose A to have y coordinates along the first axis and x coordinates
+    # Transpose Z to have y coordinates along the first axis and x coordinates
     # along the second axis
-    A = A.transpose()
+    Z = Z.transpose()
 
     # Call underlying interpolation routine and return
-    res = interpolate2d(x, y, A, points, mode=mode, bounds_error=bounds_error)
+    res = interpolate2d(x, y, Z, points, mode=mode, bounds_error=bounds_error)
     return res
 
 
-def check_inputs(x, y, A, points, mode, bounds_error):
+def check_inputs(x, y, Z, points, mode, bounds_error):
     """Check inputs for interpolate2d function
     """
 
@@ -172,15 +173,15 @@ def check_inputs(x, y, A, points, mode, bounds_error):
     assert max(y) == y[-1], msg
 
     try:
-        A = numpy.array(A)
-        m, n = A.shape
+        Z = numpy.array(Z)
+        m, n = Z.shape
     except Exception, e:
-        msg = 'A must be a 2D numpy array: %s' % str(e)
+        msg = 'Z must be a 2D numpy array: %s' % str(e)
         raise Exception(msg)
 
     Nx = len(x)
     Ny = len(y)
-    msg = ('Input array A must have dimensions %i x %i corresponding to the '
+    msg = ('Input array Z must have dimensions %i x %i corresponding to the '
            'lengths of the input coordinates x and y. However, '
            'A has dimensions %i x %i.' % (Nx, Ny, m, n))
     assert Nx == m, msg
@@ -212,4 +213,77 @@ def check_inputs(x, y, A, points, mode, bounds_error):
         if eta[-1] > y[-1]:
             raise Exception(msg)
 
-    return x, y, A, xi, eta
+    return x, y, Z, xi, eta
+
+"""
+Bilinear interpolation is based on the standard 1D linear interpolation
+formula:
+
+Given points (x0, y0) and (x1, x0) and a value of x where x0 <= x <= x1,
+the linearly interpolated value y at x is given as
+
+alpha*(y1-y0) + y0
+
+or
+
+alpha*y1 + (1-alpha)*y0                (1)
+
+where alpha = (x-x0)/(x1-x0)           (1a)
+
+
+2D bilinear interpolation aims at obtaining an interpolated value z at a point
+(x,y) which lies inside a square formed by points (x0, y0), (x1, y0),
+(x0, y1) and (x1, y1) for which values z00, z10, z01 and z11 are known.
+
+This obtained be first applying equation (1) twice in in the x-direction
+to obtain interpolated points q0 and q1 for (x, y0) and (x, y1), respectively.
+
+q0 = alpha*z10 + (1-alpha)*z00         (2)
+
+and
+
+q1 = alpha*z11 + (1-alpha)*z01         (3)
+
+
+Then using equation (1) in the y-direction on the results from (2) and (3)
+
+z = beta*q1 + (1-beta)*q0              (4)
+
+where beta = (y-y0)/(y1-y0)            (4a)
+
+
+Substituting (2) and (3) into (4) yields
+
+z = alpha*beta*z11 + beta*z01 - alpha*beta*z01 +
+    alpha*z10 + z00 - alpha*z00 - alpha*beta*z10 - beta*z00 +
+    alpha*beta*z00
+  = alpha*beta*(z11 - z01 - z10 + z00) +
+    alpha*(z10 - z00) + beta*(z01 - z00) + z00
+
+which can be further simplified to
+
+z = alpha*beta*(z11 - dx - dy - z00) + alpha*dx + beta*dy + z00  (5)
+
+where
+dx = z10 - z00
+dy = z01 - z00
+
+Equation (5) is what is implemented in the function interpolate2d above.
+
+
+Piecewise constant interpolation can be implemented using the same coefficients
+(1a) and (4a) that are used for bilinear interpolation as they are a measure of
+the relative distance to the left and lower neigbours. A value of 0 will pick
+the left or lower bound whereas a value of 1 will pick the right or higher
+bound. Hence z can be assigned to its nearest neigbour as follows
+
+    | z00   alpha < 0.5 and beta < 0.5    # lower left corner
+    |
+    | z10   alpha >= 0.5 and beta < 0.5   # lower right corner
+z = |
+    | z01   alpha < 0.5 and beta >= 0.5   # upper left corner
+    |
+    | z11   alpha >= 0.5 and beta >= 0.5  # upper right corner
+
+
+"""
